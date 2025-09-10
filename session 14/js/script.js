4ad804039213c9a3557cfd0a23f29c77dc56ff96
KhaledOsama0 <khaledosama22222@gmail.com>
2025-09-10 20:23:43 +0300

    task session 14 (form-task)
    Signed-off-by: KhaledOsama0 <khaledosama22222@gmail.com>

---
+++ session 14/js/script.js
@@ -0,0 +1,85 @@
+function changeImg()
+{
+    var img1=document.querySelector("#img")
+    if (img1.src.endsWith("images/524811586_1293709285640683_2960591745364229270_n.jpg"))
+    {
+        img1.src="images/download.jpg"
+    }
+    else
+    {
+    img1.src="images/524811586_1293709285640683_2960591745364229270_n.jpg"}
+    
+}
+var userForm = document.getElementById('userForm')
+var users = []
+
+userForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(e)
+    console.log(e.target.elements.userName.value)
+    console.log(e.target.elements.userEmail.value)
+
+    var user = {
+        name: e.target.elements.userName.value,
+        email: e.target.elements.userEmail.value
+    }
+
+    users.push(user)
+    console.log(users)
+
+    showUser()
+   
+})
+
+
+
+var showUser = () => {
+    var tBody = document.getElementById('tableBody')
+    console.log(users)
+    tBody.innerText = ""
+    users.forEach((item , i )=>{
+        var tr = document.createElement("tr")
+        var td = document.createElement('td')
+        td.innerText = item.name
+        var td2 = document.createElement('td')
+        var td3 = document.createElement('td')
+        td2.innerText = item.email
+
+        var editBtn = document.createElement('button')
+        editBtn.innerText = "Edit user"
+        editBtn.classList.add('btn' , 'btn-success' , 'btn-sm')
+        editBtn.addEventListener('click' , ()=>{
+    var newName = prompt("Enter new name", users[i].name);
+    var newEmail = prompt("Enter new email", users[i].email);
+
+    if (newName) {
+        users[i].name = newName;
+    }
+    if (newEmail) {
+        users[i].email = newEmail;
+    }
+    showUser();
+        })
+        var deleteBtn = document.createElement('button')
+        deleteBtn.innerText = "Remove user"
+        deleteBtn.classList.add('btn' , 'btn-danger' ,  "ms-4")
+       
+          deleteBtn.addEventListener('click', () => {
+            users.splice(i, 1)  
+            showUser()           
+        })
+        td3.append(editBtn)
+        td3.append(deleteBtn)
+
+        tr.append(td)
+        tr.append(td2)
+        tr.append(td3)
+
+        tBody.append(tr)
+    })
+
+
+
+
+
+}
